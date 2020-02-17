e5ae0a8a411e16a26dd410ca6c1b3371345240b8
Remiqdm <remihinschberger@gmail.com>
2020-02-17 15:03:08 +0100

    ajout de la page contact

---
+++ contact.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html> 
+ <html>      
+	  <head>       
+	    <title>CPPiades 2020</title>
+	    <meta charset="utf-8" /> 
+	    <link rel="stylesheet" href="teteetpied.css"/>    
+	 	<link rel="stylesheet" href="page.css"/>
+	  </head>
+	    
+	  <body>
+
+	  		 <div>
+         		 <?php include("include/tete.php") ?>
+       		 </div>
+	  	
+	  		 <div><h5>Contacts</h5></div>
+
+
+	  		 <div class="numcontact">
+		  			<p class="num">Jules Marmier (Président): 06 75 63 16 20     <br>
+		  				Léo Bertrand(Vice Président): 06 78 02 32 30<br>
+		  				Amélie (Secrétaire): 07 81 29 37 41 <br>
+		  			   Allan Lancezeu (Trésorier): 06 46 25 73 44   <br>
+		  			   Coline Servantie (Tresorière): 06 52 49 19 74
+		  			</p>
+		  			<!--<p id="organigramme_dl"><a href="img/organigramme_aaoic.pdf" target="_blank" class="link">Téléchargez l'organigramme complet</a></p>-->
+				
+			 </div>
+	  		 
+			 <div>
+	   				<?php include("include/pied.php") ?>
+	   		 </div>
+	  </body>
+
+ </html>
