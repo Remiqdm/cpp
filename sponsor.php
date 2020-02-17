09ceec4eff406b1482aca27127c1270e8b5fa33d
Remiqdm <remihinschberger@gmail.com>
2020-02-17 14:51:45 +0100

    rajout de fichiers

---
+++ sponsor.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html> 
+ <html>      
+  <head>       
+    <title>CPPiades 2020</title>
+    <meta charset="utf-8" /> 
+    <link rel="stylesheet" href="teteetpied.css"/>    
+    <link rel="stylesheet" href="page.css"/>
+  </head>
+
+
+   <body>
+   	  <div>
+          <?php include("include/tete.php") ?>
+      </div>
+
+      <div class="sponsor">
+
+                <div><p><h4>Nos Sponsors</h4></p></div>
+
+                <div class="logo">
+
+
+                        <div class="cartesponso">
+                            <div><img src="img/climbup.png" alt=""/></div>
+                            <div><p class="sponso">text a dire</p></div>
+                        </div>
+
+                        <div class="cartesponso">
+                            <div><p class="sponso">text a dire</p></div>
+                            <div><img src="img/fgi.png" alt=""/></div>
+                        </div>
+
+                        <div class="cartesponso">
+                            <div><img src="img/climbup.png" alt=""/></div>
+                            <div><p class="sponso">text a dire</p></div>
+                        </div>
+
+                </div>
+      </div>
+
+      <div>
+        <?php include("include/pied.php") ?>
+      </div>
+
+   </body>
+   
+</html>
